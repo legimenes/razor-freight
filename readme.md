879757540d79d2fc20050675900ffb80c8389dac
Leandro G <leandru@gmail.com>
2023-10-14 23:48:04 -0300

    feat: criando a aplicação

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Razor Freight
+
+Aplicação acadêmica para cálculo de frete desenvolvida em ASP.NET Core Razor Pages.
---
+++ src/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace RazorFreight.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
